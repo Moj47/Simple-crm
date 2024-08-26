b1dadb5f8e4db9c83374da3879136ec67e9343cb
Moji <472306m@gmail.com>
2024-08-26 10:08:19 +0330

    Migration with Models relation

---
+++ app/Models/Task.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Task extends Model
+{
+    use SoftDeletes;
+
+    use HasFactory;
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    protected $fillabel=['name','description','deadline','status'];
+
+}
