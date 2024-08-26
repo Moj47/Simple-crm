aba9317c13dfa9588f8d6ad2edffc3d74f836ef3
Moji <472306m@gmail.com>
2024-08-26 12:18:01 +0330

    simple index method of projects--edit in project factory to assign user and client for project

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Client;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -17,9 +19,13 @@ class ProjectFactory extends Factory
      */
     public function definition(): array
     {
+        $user=User::factory()->create();
+        $client=Client::factory()->create();
         return [
             'title'=>$this->faker->title,
             'description'=>$this->faker->text,
+            'user_id'=>$user->id,
+            'client_id'=>$client->id,
             'deadline'=>$this->faker->date(),
             'status'=>Arr::random(['open','close'])
         ];
