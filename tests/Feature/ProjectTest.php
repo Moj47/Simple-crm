fcba2fa34c0398b129436e95814a0e34ada03273
Moji <472306m@gmail.com>
2024-09-04 13:53:24 +0330

    Update Project Test

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -2,19 +2,32 @@
 
 namespace Tests\Feature;
 
+use App\Models\Project;
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProjectTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+  use RefreshDatabase;
     public function testProjectIndexShowAllProjects(): void
     {
         $response=$this->get(route('projects.index'));
         $response->assertOk();
         $response->assertViewHas('projects');
+    }
+    public function testProjectEditWithUpdateMethod()
+    {
+        $project=Project::factory()->create();
+        $task=Task::factory()->count(rand(1,6))->create(['user_id'=>$project->user_id,'client_id'=>$project->client_id,'project_id'=>$project->id]);
+        $this->assertDatabaseHas('projects',$project->toArray());
+
+        $project2=Project::factory()->make();
+
+        $this->put(route('projects.update',$project),
+        $project2->toArray())->assertRedirect(route('projects.index'));
+        $this->assertDatabaseHas('projects',$project2->toArray());
+
     }
 }
