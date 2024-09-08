1869fad59dd9bd16cb9a25f3dbbe2f69ba4c0711
Moji <472306m@gmail.com>
2024-09-08 11:42:07 +0330

    Projects tests:soft delete-delete with tasks-Delete withouts tasks

--- tests/Feature/ProjectTest.php
+++ tests/Feature/ProjectTest.php
@@ -2,11 +2,12 @@
 
 namespace Tests\Feature;
 
-use App\Models\Project;
-use App\Models\Task;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Task;
+use App\Models\User;
+use App\Models\Project;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProjectTest extends TestCase
 {
@@ -19,23 +20,74 @@ class ProjectTest extends TestCase
     }
     public function testCreateWithStoreMethod()
     {
+        $user=User::factory()->create(['type'=>'admin']);
         $project=Project::factory()->make();
-        $this->post(route('projects.store',$project),
+        $this->actingAs($user)
+        ->post(route('projects.store',$project),
         $project->toArray())->assertRedirect(route('projects.index'));
         $this->assertDatabaseHas('projects',$project->toArray());
 
     }
     public function testProjectEditWithUpdateMethod()
     {
+        $user=User::factory()->create(['type'=>'admin']);
         $project=Project::factory()->create();
         $task=Task::factory()->count(rand(1,6))->create(['user_id'=>$project->user_id,'client_id'=>$project->client_id,'project_id'=>$project->id]);
         $this->assertDatabaseHas('projects',$project->toArray());
 
         $project2=Project::factory()->make();
 
-        $this->put(route('projects.update',$project),
+        $this->actingAs($user)->put(route('projects.update',$project),
         $project2->toArray())->assertRedirect(route('projects.index'));
         $this->assertDatabaseHas('projects',$project2->toArray());
 
+    }
+    public function testProjectSoftDeleteWithDestroyMethod()
+    {
+        $user=User::factory()->create(['type'=>'admin']);
+        $project=Project::factory()->create();
+        $task=Task::factory()->count(rand(1,6))->create(['user_id'=>$project->user_id,'client_id'=>$project->client_id,'project_id'=>$project->id]);
+
+        $response=$this->actingAs($user)
+        ->delete(route('projects.destroy',$project))
+        ->assertRedirect(route('projects.index'));
+        $this->assertDatabaseHas($project);
+
+    }
+    public function testProjectForceDeleteWithDestroyMethodWhileItHasTasks()
+    {
+        $user=User::factory()->create(['type'=>'admin']);
+        $project=Project::factory()->create();
+        $task=Task::factory()->count(rand(1,6))->create(['user_id'=>$project->user_id,'client_id'=>$project->client_id,'project_id'=>$project->id]);
+
+        $response=$this->actingAs($user)
+        ->delete(route('projects.destroy',$project))
+        ->assertRedirect(route('projects.index'));
+        $this->assertDatabaseHas($project);
+
+        //now lets for force delete
+
+        $this->actingAs($user)
+        ->delete(route('projects.force-delete',$project->id))
+        ->assertRedirect()
+        ->assertSessionHas('status','Project belongs to task. Cannot delete.');
+        $this->assertDatabaseHas($project);
+    }
+    public function testProjectForceDeleteWithDestroyMethodWhileItHasNotAnyTask()
+    {
+        $user=User::factory()->create(['type'=>'admin']);
+        $project=Project::factory()->create();
+
+        $response=$this->actingAs($user)
+        ->delete(route('projects.destroy',$project))
+        ->assertRedirect(route('projects.index'));
+        $this->assertDatabaseHas($project);
+
+        //now lets for force delete
+
+        $this->actingAs($user)
+        ->delete(route('projects.force-delete',$project->id))
+        ->assertRedirect();
+        $this->assertDatabaseMissing($project);
     }
 }
